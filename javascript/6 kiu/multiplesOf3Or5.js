60dc522ff65003b4162a27b372dee2bfebb7bb51
Diego Ramos <diego.ramos@catho.com>
2019-01-11 14:36:36 -0200

    multiples of 3 or 5 - done

--- javascript/6 kiu/multiplesOf3Or5.js
+++ javascript/6 kiu/multiplesOf3Or5.js
@@ -8,9 +8,12 @@ function solution(number) {
         i % 3 === 0 ? multiplesOfThree.push(i) : false;
         i % 5 === 0 ? multiplesOfFive.push(i) : false;
     }
-    let noRepeatArray = multiplesOfFive.concat(multiplesOfThree).filter((value, index) => multiplesOfFive.concat(multiplesOfThree).indexOf(value) == index)
-    return noRepeatArray.reduce((accumulated, current) => accumulated + current)
-}
-
+    let noRepeatArray = multiplesOfFive.concat(multiplesOfThree).filter((value, index) => multiplesOfFive.concat(multiplesOfThree).indexOf(value) == index);
+    let arraySum = 0;
+    for (let i = 0; i < noRepeatArray.length; i++) {
+        arraySum += noRepeatArray[i];
+    };
+    return arraySum
+};
 
 console.log(solution(10))

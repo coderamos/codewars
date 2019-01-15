0dfea2fb9aa4d2a223ad6abc9dedae53697f0ac0
Diego Ramos <diego.ramos@catho.com>
2019-01-15 16:14:10 -0200

    delete comments

--- javascript/7 kiu/done - shortestWord.js
+++ javascript/7 kiu/done - shortestWord.js
@@ -4,15 +4,3 @@ function findShort(text) {
     return text.split(' ').map((value) => value.length).sort((a, b) => a - b)[0];
 }
 console.log(findShort('bitcoin 11 11122233312 take over the world maybe who knows perhaps'));
-
-// arr = [ 7, 4, false, 3, false, false, false, false, false ]
-// console.log(arr.sort())
-
-// str = 'bitcoin take 11111111111 over the world maybe who knows perhaps';
-// newArr = str.split(' ').map(function (value) {
-//     return value.length
-// })
-
-// console.log(newArr.sort(function (a, b) {
-//     return a - b
-// }))
